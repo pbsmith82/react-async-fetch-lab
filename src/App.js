d6e91d8a255939e3339d7c8f684ed4aacf2c4c77
pbsmith82 <pbsmith82@gmail.com>
2021-05-23 12:44:35 -0400

    commit tree afbb95ca89cb4ed822fd5fd8a879235b86c409f8

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
-// create your App component here
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+
+    componentDidMount(){
+        let endpoint = 'http://api.open-notify.org/astros.json';
+
+        fetch(endpoint)
+            .then(res => res.json())
+            
+    }
+
+    render(){
+        return(
+            ''
+        )
+
+    }
+
+
+}
