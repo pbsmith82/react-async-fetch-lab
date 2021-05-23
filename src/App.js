486c81c2ee82df9ab8a8b1f0c24d83906827b132
pbsmith82 <pbsmith82@gmail.com>
2021-05-23 13:11:32 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,33 @@
-// create your App component here
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+    state = {
+        json:''
+    }
+
+
+    componentDidMount(){
+        let endpoint = 'http://api.open-notify.org/astros.json';
+
+        fetch(endpoint)
+            .then(res => res.json())
+            .then(jsondata => this.setState({json: jsondata}))
+            
+    }
+
+    render(){
+        if(this.state.json.people){
+            const peopleCards = this.state.json.people.map(people => (people.name))
+            return (<div className="ui cards"> {peopleCards.map(card => card + ' - ' )} </div>)
+        }else{
+            return(
+                <div>
+                    
+                </div>
+                )
+        }
+    }
+
+
+}
